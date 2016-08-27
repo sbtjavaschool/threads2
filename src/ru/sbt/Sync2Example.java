b0f8622a9fa412a93a66015a401d6bf164fc7eaa
sbtjavaschool <sbt.java.school@gmail.com>
2016-08-27 12:39:36 +0300

    .

---
+++ src/ru/sbt/Sync2Example.java
@@ -0,0 +1,39 @@
+package ru.sbt;
+
+public class Sync2Example {
+    private volatile boolean finish = false;
+    private volatile int counter;
+
+    public void doLoop() {
+        synchronized (this) {
+            while (!finish) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        System.out.println(counter);
+    }
+
+    public void stop(int counter) {
+        this.counter = counter;
+        finish = true;
+
+        synchronized (this) {
+            notify();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Sync2Example example = new Sync2Example();
+
+        new Thread(example::doLoop).start();
+
+        Thread.sleep(2000);
+
+        new Thread(() -> example.stop(500)).start();
+    }
+}
