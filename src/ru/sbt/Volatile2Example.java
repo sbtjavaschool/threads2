9afd64c08b4ddf1c09fc8d08d5c53b98ceb6cc31
sbtjavaschool <sbt.java.school@gmail.com>
2016-08-27 12:14:04 +0300

    .

---
+++ src/ru/sbt/Volatile2Example.java
@@ -0,0 +1,23 @@
+package ru.sbt;
+
+public class Volatile2Example {
+    private volatile Person person = new Person("Bob", 12);
+
+    public void changePerson(String name, int x) {
+        person = new Person(name, x);
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Volatile2Example example = new Volatile2Example();
+
+        new Thread(()-> example.changePerson("Alex", 12)).start();
+
+        Thread.sleep(1_000);
+
+        new Thread(()-> System.out.println(example.getPerson().getName())).start();
+    }
+}
